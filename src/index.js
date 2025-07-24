b8198b23643a8c7794de43df787d3b563bccb340
abnadabcastro <55atende.abnadabcastro@gmail.com>
2025-07-24 17:50:06 -0300

    meu primeiro projeto nodejs dio - continuacao

---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log("hello world regina duarte");
+
+// ls -> lista de arquivos do diretório
+// cls -> limpa terminal
+// delele -> del *
+// cd  -> muda o diretorio "cd .. volta diretorio"
+// mkdir -> cria um diretorio novo
+// clear -> limpa terminal
+// code -> abre o vscode
+
+//comandos node//
+
+//npm init -y -> inicia um projeto novo
+//node -> roda aequivo javascript
+//node --watch [aequivo] -> node fica vigiando arquivo
